819a2975d5d4c24b9bdbc38458f4ad7947efa069
onedss <onedss@qq.com>
2022-05-06 13:30:27 +0800

    add files.

---
+++ app/src/main/java/com/xyd/lametest/MainActivity.java
@@ -0,0 +1,36 @@
+package com.xyd.lametest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.xyd.lametest.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'lametest' library on application startup.
+    static {
+        System.loadLibrary("lametest");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'lametest' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
