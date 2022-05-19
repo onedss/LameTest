bfd38f8a199c9d43cb1535816b37776f01e273a7
onedss <onedss@qq.com>
2022-05-19 21:27:03 +0800

    add files.

---
+++ app/src/main/cpp/lame_util.c
@@ -0,0 +1,13 @@
+#include <jni.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,26 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
+#ifdef __cplusplus
+extern "C"
+{
+#include "lame.h"
+#endif
+JNIEXPORT jstring JNICALL
 Java_com_xyd_lametest_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+JNIEXPORT jstring JNICALL
+Java_com_xyd_lametest_MainActivity_getLameVersion(
+        JNIEnv* env,
+jobject /* this */) {
+    std::string hello = "Hello from Lame";
+    return env->NewStringUTF(get_lame_version());
+}
+#ifdef __cplusplus
+}
+#endif
